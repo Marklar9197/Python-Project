410d055baf0c88bc36ed14350a5c1839f16ceb46
Brandon Goldenberg <marklar9197@yahoo.com>
2016-03-28 15:51:02 -0400

    add functions

--- game.py
+++ game.py
@@ -8,20 +8,32 @@
 
 import random
 
+
 random_num = random.randint(1,10)
 
 print(random_num)
 
-print("I'm thinking of a number between 1 and 10")
+def number_choice():
+	print("I'm thinking of a number between 1 and 10")
 user_guess = int(input("What is my number? > "))
 
 if user_guess < random_num:
-	print("Please try a larger number")
+	int(input("Please try a larger number"))
 
 elif user_guess > random_num:
-	("Please try a smaller number")
+	int(input("Please try a smaller number"))
 
 elif user_guess == random_num:
-	print("You got it!")
+	print "You got it!"
+
+
+def replay():
+replay = input("Would you like to play again? > ")
+
+if replay == 'yes'.lower():
+	number_choice()
+
+
+
 
 
