ad4111f45529056e3d60d64b92e93805b80bd97e
Brandon Goldenberg <marklar9197@yahoo.com>
2016-03-28 15:01:33 -0400

    add randrage and import random

--- game.py
+++ game.py
@@ -1,1 +1,17 @@
-print(input("Would you like to play a game?"))
+# question = input("Would you like to play a game?")
+
+# if question == 'YES':
+# 	print("Ok let's begin")
+
+# else:
+# 	print('????')
+
+import random
+
+random.randrange(100, 11)
+
+
+print("I'm thinking of a number between 1 and 10")
+print(input("What is my number?"))
+
+
