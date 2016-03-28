dc94b8b63a1ab261fc681ff35183e15fafeffe4f
Brandon Goldenberg <marklar9197@yahoo.com>
2016-03-28 15:23:24 -0400

    add if and elif statements

--- game.py
+++ game.py
@@ -15,6 +15,13 @@ print(random_num)
 print("I'm thinking of a number between 1 and 10")
 user_guess = input("What is my number? > ")
 
+if user_guess < random_num:
+	print("Please try a larger number")
 
+elif user_guess > random_num:
+	("Please try a smaller number")
+
+elif user_guess == random_num:
+	print("You got it!")
 
 
