1b74ebbf7be34107c75fddf2ff28fb58fda25b77
Brandon Goldenberg <marklar9197@yahoo.com>
2016-03-28 16:11:07 -0400

    write callback functions and comments

--- game.py
+++ game.py
@@ -11,27 +11,33 @@ import random
 
 random_num = random.randint(1,10)
 
-print(random_num)
+print(random_num) # Being used for testing purposes
 
+
+#function to ask for a user's input
 def number_choice():
 	print("I'm thinking of a number between 1 and 10")
 user_guess = int(input("What is my number? > "))
 
 if user_guess < random_num:
-	int(input("Please try a larger number"))
+	int(input("Please try a larger number > "))
+	number_choice()
 
 elif user_guess > random_num:
-	int(input("Please try a smaller number"))
+	int(input("Please try a smaller number > "))
+	number_choice()
 
 elif user_guess == random_num:
 	print "You got it!"
+	replay()
 
 
 def replay():
-replay = input("Would you like to play again? > ")
+	replay = input("Would you like to play again? > ")
 
 if replay == 'yes'.lower():
 	number_choice()
+
 
 
 
