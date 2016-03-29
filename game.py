9df43f298ea3378b37d370b67de298c5730bb2d4
Brandon Goldenberg <marklar9197@yahoo.com>
2016-03-29 10:30:07 -0400

    Change function names

--- game.py
+++ game.py
@@ -1,10 +1,5 @@
-# question = input("Would you like to play a game?")
+#This program takes a random number between 1 and 10 and asks the user to guess it.
 
-# if question == 'YES':
-# 	print("Ok let's begin")
-
-# else:
-# 	print('????')
 
 import random
 
@@ -14,22 +9,24 @@ random_num = random.randint(1,10)
 print(random_num) # Being used for testing purposes
 
 
-#function to ask for a user's input
-def number_choice():
+def greeting():
 	print("I'm thinking of a number between 1 and 10")
 user_guess = int(input("What is my number? > "))
 
-if user_guess < random_num:
+
+
+def user_choice():
+	if user_guess < random_num:
 	int(input("Please try a larger number > "))
-	number_choice()
+	return 
 
 elif user_guess > random_num:
 	int(input("Please try a smaller number > "))
-	number_choice()
 
 elif user_guess == random_num:
 	print "You got it!"
 	replay()
+
 
 
 def replay():
@@ -37,6 +34,13 @@ def replay():
 
 if replay == 'yes'.lower():
 	number_choice()
+
+
+
+
+
+
+
 
 
 
